4fecc8af79bfdd048aa4a3e53766c5d2398bf58f
Poyoyo <108239413+HusseinGazouini@users.noreply.github.com>
2024-08-21 10:45:43 -0300

    Update main.py

--- TDE conjuntos/main.py
+++ TDE conjuntos/main.py
@@ -1,9 +1,6 @@
 from itertools import product
 
-
-
 file = input("Digite qual txt File deseja usar... (data.txt, data1.txt, data2.txt): ")
-
 
 def read(file):
     with open(file, 'r') as f:
@@ -12,33 +9,25 @@ def read(file):
     num_operation = int(lines[0])
     i = 1
 
-    for j in range(num_operation):
- 
-
+    while num_operation > 0:
         operation = lines[i].strip()
         set1 = set(lines[i + 1].split(','))
         set2 = set(lines[i + 2].split(','))
         i += 3
 
-
         if operation == 'U':
-            resultado = set1 | set2  
-            print(f"União: conjunto 1 {set1}, conjunto 2 {set2}. Resultado: {resultado}.")
-
+            print(f"União: conjunto 1 {set1}, conjunto 2 {set2}. Resultado: {set1 | set2}.")
 
         elif operation == 'I':
-            resultado = set1 & set2  
-            print(f"Interseção: conjunto 1 {set1}, conjunto 2 {set2}. Resultado: {resultado}.")
-
+            print(f"Interseção: conjunto 1 {set1}, conjunto 2 {set2}. Resultado: {set1 & set2}.")
 
         elif operation == 'D':
-            resultado = set1 - set2  
-            print(f"Diferença: conjunto 1 {set1}, conjunto 2 {set2}. Resultado: {resultado}.")
+            print(f"Diferença: conjunto 1 {set1}, conjunto 2 {set2}. Resultado: {set1 - set2}.")
 
 
         elif operation == 'C':
-            resultado = set(product(set1, set2))  # produc "multiplicar"
-            print(f"Produto cartesiano: conjunto 1 {set1}, conjunto 2 {set2}. Resultado: {resultado}.")
-
+            print(f"Produto cartesiano: conjunto 1 {set1}, conjunto 2 {set2}. Resultado: {set(product(set1, set2))}.")
+        
+        num_operation -= 1
 
 read(file)
